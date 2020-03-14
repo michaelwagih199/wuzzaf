592aeebbc9adbb86d81068ea4bd3f132e4d03533
omelnour <michaelwagih199@gmail.com>
2020-03-14 10:48:22 +0200

    14-3-

--- app/src/main/java/com/example/wuzzaf/activities/JobSuggestionDetails.kt
+++ app/src/main/java/com/example/wuzzaf/activities/JobSuggestionDetails.kt
@@ -3,7 +3,6 @@ package com.example.wuzzaf.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
--- app/src/main/java/com/example/wuzzaf/activities/Login.java
+++ app/src/main/java/com/example/wuzzaf/activities/Login.java
@@ -1,15 +1,19 @@
 package com.example.wuzzaf.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.wuzzaf.R;
 import com.example.wuzzaf.dao.UsersFirestoreManager;
@@ -23,6 +27,8 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     TextView txtVSignIn;
     private EditText etUserName, etPassword;
     private UsersFirestoreManager usersFirestoreManager;
+    private ImageView imgPasswordEdit;
+    boolean visable = false;
 
 
     @Override
@@ -39,7 +45,12 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         txtVSignIn.setOnClickListener(this);
         etUserName = (EditText) findViewById(R.id.etUserName);
         etPassword = (EditText) findViewById(R.id.etPassword);
+        imgPasswordEdit = (ImageView) findViewById(R.id.imgPasswordEdit);
+        imgPasswordEdit.setOnClickListener(this);
         usersFirestoreManager = UsersFirestoreManager.newInstance();
+
+
+
 
     }
 
@@ -67,9 +78,24 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                     break;
 
                 case R.id.txtVSignIn:
+
                     Intent i = new Intent(this, Signup.class);
                     startActivity(i);
                     break;
+
+                case R.id.imgPasswordEdit:
+                    visable = true;
+                    if (visable) {
+                        etPassword.setTransformationMethod(null);
+                        imgPasswordEdit.setImageDrawable(getResources().getDrawable(R.drawable.ic_visibility_off_black_24dp));
+
+                    } else {
+                        etPassword.setTransformationMethod(new PasswordTransformationMethod());
+                        imgPasswordEdit.setImageDrawable(getResources().getDrawable(R.drawable.ic_visibility_white_24dp));
+                    }
+
+                    break;
+
 
             }
 
